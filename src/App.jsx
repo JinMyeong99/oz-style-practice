4e97e831b03c167e271a30a4260903217049bdfb
JinMyeong Choi <ymo6538@gmail.com>
2025-11-19 13:50:20 +0900

    완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+const Main = styled.main``;
+
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
-    <main>
+    <Main>
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
-    </main>
+      </Section>
+    </Main>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 20px;
+  background-color: black;
+  color: white;
+`;
+
+const MenuList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  list-style: none;
+`;
+
+const MenuItem = styled.li`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <HeaderContainer>
       <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
+      <MenuList>
+        <MenuItem>로그인</MenuItem>
+        <MenuItem>회원가입</MenuItem>
+        <MenuItem>내클래스</MenuItem>
+      </MenuList>
+    </HeaderContainer>
   );
 }
